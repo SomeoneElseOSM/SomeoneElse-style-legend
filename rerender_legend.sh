e1c3abb355c14ef659344b61cc9f258fd4432bf2
Andy Townsend <ajtown@atownsend.org.uk>
2017-08-09 23:28:08 +0100

    Added script to rerender the legend.

---
+++ rerender_legend.sh
@@ -0,0 +1,3 @@
+PATH=/home/renderaccount/bin:/home/renderaccount/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+/home/renderaccount/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 1 -Z 17 -x 135 -X 135.2 -y -24.9 -Y -25.1 -m ajt -f 1
+

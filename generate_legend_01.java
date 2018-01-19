0317556cae5b7b02de55543d734c3b0ca4a68dce
Andy Townsend <ajtown@atownsend.org.uk>
2018-01-19 12:56:46 +0000

    Added support for illumination masts.

--- generate_legend_01.java
+++ generate_legend_01.java
@@ -1406,6 +1406,11 @@ public class generate_legend_01
 				 "name", "Name" );
 
 	currentCol += 1;
+	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Illumination", 
+				 "man_made", "illuminationtower", 
+				 "name", "Name" );
+
+	currentCol += 1;
 	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Water Tower", 
 				 "man_made", "water_tower", 
 				 "name", "Name" );

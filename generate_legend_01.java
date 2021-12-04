313f5be5208aa426a3f09919fc96d7c12806ab2f
Andy Townsend <ajtown@atownsend.org.uk>
2021-12-04 10:36:37 +0000

    Added flagpoles (non-military and military) to legend.

--- generate_legend_01.java
+++ generate_legend_01.java
@@ -1976,6 +1976,16 @@ public class generate_legend_01
 				 "name", "Name" );
 
 	currentCol += 1;
+	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Flagpole", 
+				 "man_made", "flagpole", 
+				 "name", "Name" );
+
+	currentCol += 1;
+	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Military", 
+				 "man_made", "flagpole_red", 
+				 "name", "Name" );
+
+	currentCol += 1;
 	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Water Tower", 
 				 "man_made", "water_tower", 
 				 "name", "Name" );

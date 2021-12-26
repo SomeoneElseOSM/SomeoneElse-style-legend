98cea030b676b3b510eff387fb8fa0b385165c7b
Andy Townsend <ajtown@atownsend.org.uk>
2021-12-26 23:06:15 +0000

    Added an icon for scooter rental and added to legend.

--- generate_legend_01.java
+++ generate_legend_01.java
@@ -1840,6 +1840,11 @@ public class generate_legend_01
 				 "name", "Name" );
 
 	currentCol += 1;
+	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Scooter Rental", 
+				 "amenity", "scooter_rental", 
+				 "name", "Name" );
+
+	currentCol += 1;
 	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Airport", 
 				 "aeroway", "aerodrome", 
 				 "iata", "IATA", 

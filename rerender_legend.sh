2030cfabde02b007beebbd19df43c2c6a7907b3f
Andy Townsend <ajtown@atownsend.org.uk>
2022-02-11 22:18:39 +0000

    Allow legend to be force rerendered on 20.04

--- rerender_legend.sh
+++ rerender_legend.sh
@@ -1,3 +1,13 @@
-PATH=/home/renderaccount/bin:/home/renderaccount/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-/home/renderaccount/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 1 -Z 17 -x 135 -X 135.2 -y -25.1 -Y -24.9 -m ajt -f 1
-
+# -----------------------------------------------------------------------------
+# rerender_legend.sh
+# -----------------------------------------------------------------------------
+#
+# The local user account we are using
+#
+local_filesystem_user=renderaccount
+local_renderd_user=renderaccount
+#
+PATH=/home/${local_filesystem_user}/bin:/home/${local_filesystem_user}/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#
+sudo -u ${local_renderd_user} /home/${local_filesystem_user}/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 1 -Z 17 -x 135 -X 135.2 -y -25.1 -Y -24.9 -m ajt -f 1
+#

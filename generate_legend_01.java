358a8d83f1b5f45f714ca9bdfda79e28d400b366
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2023-04-07 13:54:47 +0100

    Added icon for historic=monastery and other similar places.

--- generate_legend_01.java
+++ generate_legend_01.java
@@ -1940,6 +1940,11 @@ public class generate_legend_01
 				 "name", "Name" );
 
 	currentCol += 1;
+	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Monastery", 
+				 "historic", "monastery", 
+				 "name", "Name" );
+
+	currentCol += 1;
 	writeLabelledNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, "Round Tower", 
 				 "historic", "tower", 
 				 "tower:type", "round_tower", 

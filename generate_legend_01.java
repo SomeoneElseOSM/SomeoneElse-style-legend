c3fca7b69b7806379f4399d83cabf7c6bc9e16c2
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2026-06-04 16:02:23 +0100

    Updated legend for shop-ticket

--- generate_legend_01.java
+++ generate_legend_01.java
@@ -1141,6 +1141,11 @@ public class generate_legend_01
 
 	currentCol += 1;
 	writeNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, 
+			 "shop", "ticket", 
+			 "name", "Ticket" );
+
+	currentCol += 1;
+	writeNodeRowCol( myOutputOsmStream, startNode, currentRow, currentCol, 
 			 "shop", "copyshop", 
 			 "name", "Copyshop, Printers" );
 
